7eafb4c3ba06eca84f2b2ebf9bbbf057e88a5b71
michael <michaelyoussef396@gmail.com>
2024-01-13 13:17:34 +1100

    filter test past

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -4,22 +4,28 @@ import Filter from "./Filter";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [inputValue, setInputValue] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("All");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  const onSearchChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-    return item.category === selectedCategory;
+  const itemsToDisplay = items.filter((item) => {
+    const filterBySearch = item.name.toLowerCase().includes(inputValue.toLowerCase());
+    const filterByCategory = selectedCategory === "All" || item.category === selectedCategory;
+
+    return filterBySearch && filterByCategory;
   });
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} search={inputValue} onSearchChange={onSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
